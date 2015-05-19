ff9cb5a8ae065220752e407dbebc284c19a0fcdf
Lukas RT <lukas.rosentreter@gmail.com>
2015-05-19 14:37:06 +0200

    cd repariert, routinen in funktionen verlagert, mit prog routine begonnen

--- Aufgabe3/lab-2/sar_shell.c
+++ Aufgabe3/lab-2/sar_shell.c
@@ -6,63 +6,125 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define DIR_LENGTH 150
+#define CMD_LENGTH 50
+#define DIR_LENGTH 200
 
-char input[50], prompt[150];
-char *command;
+char input[CMD_LENGTH], prompt[DIR_LENGTH];
+
+char *command, *cmd_args;
+char has_args;
+
 char *work_dir, *base_dir;
 
-char* command_b;
+
+void cd_routine();
+void wait_routine();
+void prog_routine();
+
 
 int main(void)
 {
-	int tmp;
+	// Variablen initialisieren, Speicher holen
 	strcpy(prompt,".");
-	command = malloc(sizeof(char)*50);
+	
+	command = malloc(sizeof(char)*CMD_LENGTH);
+	has_args = 0;
+
 	work_dir = malloc(sizeof(char)*DIR_LENGTH);
 	base_dir = malloc(sizeof(char)*DIR_LENGTH);
 	getcwd(base_dir,DIR_LENGTH);
+
 	while (1) {
+		// Kommando holen
 		printf("%s> ", prompt);
-		fgets(input, 50,stdin);
+		fgets(input, CMD_LENGTH, stdin); // Wozu der Umweg ueber input?
 		command = input;
 		command[strlen(command)-1] = '\0';
 		printf("Command: %s\n", command);
-		if ((command_b = strchr(command, ' ')) != NULL) {
-			//String mit Leerzeichen
-			printf("String mit Leerzeichen\n");
-			*command_b = '\0';
-			printf("Command: %s\n", command);
 
-			/*
-			 * cd Routine
-			 */
-			if (!strcmp(command, "cd")) {
-				command = command_b+1;
-				if (!chdir(command)) {
-					getcwd(work_dir, DIR_LENGTH);
-					tmp = strcmp(base_dir, work_dir);
-					if (tmp == 0)
-						strcpy(prompt, ".");
-					else if (tmp < 0){
-						strcat(prompt, work_dir+strlen(base_dir));
-					}
-					else {
-						strcpy(prompt, work_dir);
-					}
-				}
-				else
-					printf("Pfad nicht gefunden\n");
-			}
-
-			/*
-			 * Wait Rountine
-			 */
+		// Kommando von eventuellen Parametern trennen
+		if ((cmd_args = strchr(command, ' ')) != NULL) {
+			// String hat Leerzeichen
+			// Kommando und Parameter durch NUL-Zeichen trennen
+			*cmd_args = '\0';
+			cmd_args++; // Pointer auf Param-Beginn verschieben
+			has_args = 1;
+		} else {
+			has_args = 0;
 		}
-		// Kein Leerzeichen
-		if (!strcmp(command, "exit")) {
+
+		// Kommando behandeln
+		
+		if (!strcmp(command, "cd")) {
+			cd_routine();
+		}
+		else if (!strcmp(command, "wait")) {
+			wait_routine();
+		}
+		else if (!strcmp(command, "exit")) {
 			exit(0);
 		}
+		else {
+			prog_routine();
+		}		
+	}
+}
 
+void cd_routine() {
+	if (has_args) {
+		// Pfad wurde uebergeben
+		if (!chdir(cmd_args)) {
+			// erstmal wechseln und dann prompt zusammenbasteln
+			getcwd(work_dir, DIR_LENGTH);
+
+			if (!strcmp(base_dir, work_dir)) {
+				// work_dir == base_dir
+				strcpy(prompt, ".");
+			} else {
+				// ist work_dir ein unterverzeichnis von base_dir?
+				if (!strncmp(base_dir, work_dir, strlen(base_dir))) {
+					// ja, dann ersetze base_dir durch "."
+					strcpy(prompt, ".");
+					strcat(prompt, work_dir+strlen(base_dir));
+				} else {
+					strcpy(prompt, work_dir);
+				}
+			}
+		} else {
+			printf("Pfad wurde nicht gefunden!\n");
+		}
+	} else {
+		// kein Pfad wurde uebergeben, also ins base_dir wechseln
+		chdir(base_dir);
+		strcpy(prompt, ".");
+	}
+}
+
+void wait_routine() {
+	// ...
+}
+
+void prog_routine() {
+	// TODO: ggf & aus den params entnehmen und selbst behandeln
+	// TODO: pipe behandeln
+
+	pid_t pid = fork();
+	if (pid == 0) {
+		// wir sind der Child-Prozess
+		printf("Child: %s\n", command);
+
+		// hier muesste jetzt sowas wie exec kommen
+		// was den eigentlichen befehl ausfuehrt
+
+		exit(0);
+	} else if (pid < 0) {
+		// fork failed
+		printf("Fork failed");
+		exit(1);
+	} else {
+		// wir sind der Parent-Prozess
+		int status;
+		waitpid(pid, &status, 0); // TODO: nur warten wenn kein & in params
+		printf("Parent: %d\n", status);
 	}
 }
